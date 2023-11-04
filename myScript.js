423f952ef33e01a2c3dea393bdb7083dd9170cec
ViktorKutsevych <149194678+ViktorKutsevych@users.noreply.github.com>
2023-11-04 15:31:12 +0200

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,76 @@
+
+
+let firstRowEl = document.querySelector('.row1');
+let arrRow1 = firstRowEl.children;
+
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+       if( i % 2 ===0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    } else {
+        arrRow1[i].style.backgroundColor = "yellow";  
+    }
+}
+
+ firstRowEl = document.querySelector('.row2');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 !== 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row3');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 == 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row4');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 !== 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row5');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 == 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row6');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 !== 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row7');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 == 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
+firstRowEl = document.querySelector('.row8');
+arrRow1 = firstRowEl.children;
+for (let i = 1 ; i < arrRow1.length-1;  i++ ) {
+     if( i % 2 !== 0) {
+        arrRow1[i].style.backgroundColor = "black";  
+    }
+    else arrRow1[i].style.backgroundColor = "yellow";
+
+}
